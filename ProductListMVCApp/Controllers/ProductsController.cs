478f7caf120a877910e2be71242150393dded99d
Edo Komljenović <edo.komljenovic@gmail.com>
2021-11-08 10:33:42 +0100

    Initial commit

---
+++ ProductListMVCApp/Controllers/ProductsController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductListMVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductListMVCApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        public ProductsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Product = new Product();
+            if (id == null)
+            {
+                //create
+                return View(Product);
+            }
+            //update
+            Product = _db.Products.FirstOrDefault(u => u.Id == id);
+            if (Product == null)
+            {
+                return NotFound();
+            }
+            return View(Product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+            if (ModelState.IsValid)
+            {
+                if (Product.Id == 0)
+                {
+                    //create
+                    _db.Products.Add(Product);
+                }
+                else
+                {
+                    _db.Products.Update(Product);
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Product);
+        }
+
+        #region API Calls
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Products.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var productFromDb = await _db.Products.FirstOrDefaultAsync(u => u.Id == id);
+            if (productFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Products.Remove(productFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
+        }
+        #endregion
+    }
+}
---
+++ ProductListMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProductListMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
